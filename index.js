ecd45b3badc2f69f8a26c330a43d8b4f9bde47ab
Andy Striph <ajstriph@me.com>
2017-10-11 18:22:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(katzDeliLine, name){
+
+  return //Their position in line. If theyre 7th in line, tell them they're 7th and not number 6.
+}
+
+function nowServing () {
+  if //There are poeple in line, return katzDeliLine.shift();//
+  else // return "There is nobody waiting to be served //"
+}
+
+function currentLine(){
+  if // There are people in the line, return "The line is currently: [i]name, [i]name"//
+  else // return "The line is currently empty"//
+
+}
