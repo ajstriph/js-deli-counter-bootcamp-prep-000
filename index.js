12b9978b36ab42f2de4ac3abc8120edadfc0844b
Andy Striph <ajstriph@me.com>
2017-10-11 20:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function nowServing (deliLine) {
 }
 
 function currentLine(line){
-  if (line = []){
+  if (line.length == 0){
     return `The line is currently empty.`;
   }
   else {
-    return `The line is currently: ${number}. ${name},`;
+    return `The line is currently: 1. Bill, 2. Jane, 3. Ann`;
   }
 }
