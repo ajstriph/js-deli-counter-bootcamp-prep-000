b7779cd514c32cfe4b3ea3e6972bdb23f217d329
Andy Striph <ajstriph@me.com>
2017-10-11 18:40:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 function takeANumber(katzDeliLine, name){
   var katzDeliLine = [];
-  return `Welcome, ${name}. You are number ${i} in line.`
+  return `Welcome, ${name}. You are number ${i} in line.`;
 }
 
 function nowServing (katzDeliLine) {
-  if (){}//There are poeple in line, return katzDeliLine.shift();//
-  else {} // return "There is nobody waiting to be served //"
+  if (katzDeliLine >= 1){
+      return `Currently serving ${name}.`name.shift();
+  }
+  else {
+      return `There is nobody waiting to be served.`;
+  } 
 }
 
 function currentLine(katzDeliLine){
-  if (){}// There are people in the line, return "The line is currently: [i]name, [i]name"//
-  else {}// return "The line is currently empty"//
-
+  if (katzDeliLine === 0){
+    return "The line is currently empty.";
+  }
+  else {
+    return `The line is currently: ${number}. ${name},`;
+  }
 }
