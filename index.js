4195b736571142c1795ada57aea4df06d43f1999
Andy Striph <ajstriph@me.com>
2017-10-11 18:28:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function takeANumber(katzDeliLine, name){
-
-  return //Their position in line. If theyre 7th in line, tell them they're 7th and not number 6.
+  var katzDeliLine = [];
+  return `Welcome, ${name}. You are number ${i} in line.`
 }
 
-function nowServing () {
+function nowServing (katzDeliLine) {
   if //There are poeple in line, return katzDeliLine.shift();//
   else // return "There is nobody waiting to be served //"
 }
 
-function currentLine(){
+function currentLine(katzDeliLine){
   if // There are people in the line, return "The line is currently: [i]name, [i]name"//
   else // return "The line is currently empty"//
 
